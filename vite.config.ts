a458ee6d879bc38b800cc6cf4654258b67914379
Valere Nzodjo <valere.kengni@orange.com>
2026-07-08 12:57:21 +0200

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +8,12 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+    css: true,
   },
   server: {
     port: 3001,
